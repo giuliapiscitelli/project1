fe0466701e1c5f6470a76b0c1ea82c2c60705f20
Giulia Piscitelli <giuliapiscitelli99@gmail.com>
2024-11-04 15:18:36 +0100

    Initial Commit

---
+++ project1/project1App.swift
@@ -0,0 +1,17 @@
+//
+//  project1App.swift
+//  project1
+//
+//  Created by Giulia Piscitelli on 04/11/24.
+//
+
+import SwiftUI
+
+@main
+struct project1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
